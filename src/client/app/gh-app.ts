c4a84b1c29798e18816cc23e65dfc3958f1bd0b1
Nicolas Payot <nicolas.payot@gmail.com>
2016-04-11 23:22:22 +0200

    refactor: extract repository component

--- src/client/app/gh-app.ts
+++ src/client/app/gh-app.ts
@@ -1,13 +1,15 @@
 import {Component, OnInit} from 'angular2/core';
 import {Response} from 'angular2/http';
-import {RepoService} from './repo.service';
-import {Repository} from './repo';
+import {RepoService} from './repo/repo.service';
+import {Repository} from './repo/repo';
+import {RepoComponent} from './repo/repo.component';
 
 @Component({
   selector: 'gh-app',
   templateUrl: 'app/gh-app.html',
-  styleUrls: ['app/gh-app.css', 'app/repo.css'],
-  providers: [RepoService]
+  styleUrls: ['app/gh-app.css'],
+  providers: [RepoService],
+  directives: [RepoComponent]
 })
 export class GhApp implements OnInit {
 
--- src/client/app/repo/repo.ts
+++ src/client/app/repo/repo.ts
@@ -1,5 +1,4 @@
 interface User {
-  id: number,
   login: string,
   html_url: string,
   avatar_url: string
